b1f3660f0b3e337dbf47f5d3cda2d74da0e520d3
iNDirection <Noah.Denier@gmail.com>
2024-08-17 18:16:02 -0400

    Add health to creatures

--- Assets/Creatures/api/Creature.cs
+++ Assets/Creatures/api/Creature.cs
@@ -4,6 +4,7 @@ namespace Creatures.Api
 {
     public abstract class Creature : MonoBehaviour
     {
+        public float Health { get; set; } = 100;
         public float Speed { get; set; } = 0.5f;
         public bool SpeedEnabled { get; set; } = true;
     }
