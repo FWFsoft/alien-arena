c2a5d057cf604521a7cb8e1a78748f6bad07ad16
iNDirection <Noah.Denier@gmail.com>
2024-08-25 17:33:49 -0400

    Remove overidden fields from alien

--- Assets/Scripts/AlienScript.cs
+++ Assets/Scripts/AlienScript.cs
@@ -6,29 +6,24 @@ using UnityEngine;
 
 public class AlienScript : Creature
 {
-    public Animator animator;
     public Animator frothyAnimator;
-    public float maximumAttackDistance = 100;
-    [SerializeField]
-    public HealthScript healthScript;
-    public bool isDead = false;
 
     private void Update()
     {
-        if (healthScript.isDead())
+        if (HealthScript.isDead())
         {
             return;
         }
         var target = findPlayerInRange();
         if(target == null)
         {
-            animator.SetBool("isMoving", false);
+            Animator.SetBool("isMoving", false);
             // Add patrol in here
         } else
         {
             // Move towards player
             // TODO: Add an attack when a certain distance away
-            animator.SetBool("isMoving", true);
+            Animator.SetBool("isMoving", true);
             var isLeft = target.transform.position.x < transform.position.x;
 
             this.transform.rotation = Quaternion.Euler(new Vector3(0f, isLeft ? 180f : 0f, 0f));
@@ -43,7 +38,7 @@ public class AlienScript : Creature
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
         GameObject closestPlayer = null;
-        float closestDistance = maximumAttackDistance;
+        float closestDistance = MaximumAttackDistance;
         foreach (GameObject player in players)
         {
             float dist = Vector2.Distance(transform.position, player.transform.position);
