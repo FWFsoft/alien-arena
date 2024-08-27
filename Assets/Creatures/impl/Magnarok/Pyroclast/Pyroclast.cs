94fe5c4fed197889e1822935fe49a3e480ae7e7d
Noah Denier <33496721+noahdenier@users.noreply.github.com>
2024-08-26 22:43:41 -0400

    Add all 12 enemies to the game and Spawn them into the main stage (#13)
    * Hydrate and link all the boys
    
    * Create all the spawn controllers for the boys
    
    * Spawn all the boys
    
    ---------
    
    Co-authored-by: iNDirection <Noah.Denier@gmail.com>

---
+++ Assets/Creatures/impl/Magnarok/Pyroclast/Pyroclast.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Creatures.Api;
+
+namespace Creatures.impl
+{
+    public class Pyroclast : Magnarok
+    {
+        
+        void Start()
+        {
+            Speed = 0.5f;
+            HealthScript = gameObject.AddComponent<HealthScript>();
+            HealthScript.health = 100;
+
+            // Add custom initialization logic here
+        }
+
+        // Optional: Override the Update method if custom logic is needed
+        protected override void Update()
+        {
+            base.Update();
+
+            // Add custom update logic here, if necessary
+        }
+    }
+}
