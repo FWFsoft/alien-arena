2383ee85ba5e95f3b6ba86d94f0756114d519fd5
iNDirection <Noah.Denier@gmail.com>
2024-09-02 16:59:53 -0400

    Add AbilityStates

---
+++ Assets/Creatures/api/abilities/states/ReadyState.cs
@@ -0,0 +1,22 @@
+using Creatures.Api;
+
+namespace Creatures.api.abilities.states
+{
+    public class ReadyState : IAbilityState
+    {
+        public void Enter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AbilityExecutionResult Execute(IPlayable playable, AbilityEvent abilityEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
