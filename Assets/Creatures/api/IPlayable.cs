7c002962ed41577d265f048416daa98801c88e5f
iNDirection <Noah.Denier@gmail.com>
2024-09-02 13:00:40 -0400

    Use normal comments instead of method comments

--- Assets/Creatures/api/IPlayable.cs
+++ Assets/Creatures/api/IPlayable.cs
@@ -6,21 +6,17 @@ namespace Creatures.Api
 {
     public interface IPlayable
     {
-        /**
-         * Basic Attack (aka LMB):
-         * - Should be unaffected by Silence
-         * - Only gated by GCD (aka no specific ability cooldown)
-         */ 
+        // Basic Attack (aka LMB):
+        // - Should be unaffected by Silence
+        // - Only gated by GCD (aka no specific ability cooldown)
         void BasicAttack(BasicAttackEvent basicAttackEvent);
         
-        /**
-         * Charged Attack (aka RMB):
-         * - Player starts charging the attack by clicking RMB
-         * - Ability gains strength as the button is held down
-         * - Ability is activated when the player releases RMB
-         * - Effects like Stuns or Silences will interrupt charging
-         * - Only gated by GCD (aka no specific ability cooldown)
-         */
+        // Charged Attack (aka RMB):
+        // - Player starts charging the attack by clicking RMB
+        // - Ability gains strength as the button is held down
+        // - Ability is activated when the player releases RMB
+        // - Effects like Stuns or Silences will interrupt charging
+        // - Only gated by GCD (aka no specific ability cooldown)
         void StartCharging(StartChargingEvent startChargingEvent);
         void InterruptCharging(InterruptChargingEvent interruptChargingEvent);
         void ChargedAttack(ChargedAttackEvent chargedAttackEvent);
