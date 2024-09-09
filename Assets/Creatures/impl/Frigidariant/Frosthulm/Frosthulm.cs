08bda835c2097af6dfa9e1562cb5f1bbe8b5dbfd
iNDirection <Noah.Denier@gmail.com>
2024-09-09 17:59:48 -0400

    Fix linting for hydrated creatures

--- Assets/Creatures/impl/Frigidariant/Frosthulm/Frosthulm.cs
+++ Assets/Creatures/impl/Frigidariant/Frosthulm/Frosthulm.cs
@@ -1,24 +1,31 @@
-using UnityEngine;
 using Creatures.Api;
+
+
+using UnityEngine;
+
 
 namespace Creatures.impl
 {
     public class Frosthulm : Frigidariant
     {
-        
+
+
         void Start()
         {
             Speed = 0.35f;
             HealthScript = gameObject.AddComponent<HealthScript>();
             HealthScript.health = 300;
 
+
             // Add custom initialization logic here
         }
+
 
         // Optional: Override the Update method if custom logic is needed
         protected override void Update()
         {
             base.Update();
+
 
             // Add custom update logic here, if necessary
         }
--- Assets/Creatures/impl/Frigidariant/Glacili/Glacili.cs
+++ Assets/Creatures/impl/Frigidariant/Glacili/Glacili.cs
@@ -1,24 +1,31 @@
-using UnityEngine;
 using Creatures.Api;
+
+
+using UnityEngine;
+
 
 namespace Creatures.impl
 {
     public class Glacili : Frigidariant
     {
-        
+
+
         void Start()
         {
             Speed = 1f;
             HealthScript = gameObject.AddComponent<HealthScript>();
             HealthScript.health = 25;
 
+
             // Add custom initialization logic here
         }
+
 
         // Optional: Override the Update method if custom logic is needed
         protected override void Update()
         {
             base.Update();
+
 
             // Add custom update logic here, if necessary
         }
