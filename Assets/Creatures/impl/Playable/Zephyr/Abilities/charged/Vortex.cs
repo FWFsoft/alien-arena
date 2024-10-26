1bb1d4b2ecc43ee3cf1bee72cf98e12154642a44
Noah Denier <noah.denier@appian.com>
2024-10-26 15:46:42 -0400

    Animations play at cursor, layered properly

--- Assets/Creatures/impl/Playable/Zephyr/Abilities/charged/Vortex.cs
+++ Assets/Creatures/impl/Playable/Zephyr/Abilities/charged/Vortex.cs
@@ -7,9 +7,18 @@ namespace Creatures.impl.Playable.Zephyr
     {
         [SerializeField]
         public GameObject vortex;
-
+        [SerializeField] 
+        private GameObject channelTornadoEffect;
+        [SerializeField] 
+        private GameObject vortexEffect;
+        [SerializeField] 
+        private GameObject smokeRingEffect;
+       
         private bool IsCharging { get; set; }
         private Animator vortexAnimator;
+        private Animator channelTornadoAnimator;
+        private Animator vortexExplosionAnimator;
+        private Animator smokeRingAnimator;
         private Camera mainCamera;
 
         public Vortex()
@@ -19,15 +28,24 @@ namespace Creatures.impl.Playable.Zephyr
 
         void Start()
         {
+
             vortexAnimator = vortex.GetComponent<Animator>();
+            channelTornadoAnimator = channelTornadoEffect.GetComponent<Animator>();
+            vortexExplosionAnimator = vortexEffect.GetComponent<Animator>();
+            smokeRingAnimator = smokeRingEffect.GetComponent<Animator>();
             mainCamera = Camera.main;
             vortex.SetActive(false);
+            channelTornadoEffect.SetActive(false);
+            vortexEffect.SetActive(false);
+            smokeRingEffect.SetActive(false);
+            Debug.Log("Vortex set to inactive initially.");
         }
 
         void Update()
         {
             if (IsCharging)
             {
+                Debug.Log("IsCharging is true, updating vortex position.");
                 UpdateVortexPosition();
             }
         }
@@ -48,8 +66,17 @@ namespace Creatures.impl.Playable.Zephyr
                 IsCharging = false;
                 vortexAnimator.SetBool("IsCharging", IsCharging);
 
+                channelTornadoEffect.SetActive(true);
+                vortexEffect.SetActive(true);
+                smokeRingEffect.SetActive(true);
+
+                channelTornadoAnimator.SetTrigger("Release");
+                vortexExplosionAnimator.SetTrigger("Release");
+                smokeRingAnimator.SetTrigger("Release");
+
+
                 //TODO: Make this the same duration as the release animations once I tune those
-                StartCoroutine(DeactivateVortexAfterDelay(2.0f));
+                StartCoroutine(DeactivateVortexAfterDelay(0.5f));
 
                 Debug.Log("Charging Released ...");
             }
@@ -72,15 +99,35 @@ namespace Creatures.impl.Playable.Zephyr
 
         private void UpdateVortexPosition()
         {
+            if (mainCamera == null)
+            {
+                Debug.LogError("mainCamera is null in UpdateVortexPosition. Cannot update position.");
+                return;
+            }
+
             Vector3 mousePosition = Input.mousePosition;
             mousePosition.z = 10.0f;
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
-            vortex.transform.position = worldPosition;
+            if (vortex != null)
+            {
+                vortex.transform.position = worldPosition;
+                channelTornadoEffect.transform.position = worldPosition;
+                vortexEffect.transform.position = worldPosition;
+                smokeRingEffect.transform.position = worldPosition;
+                Debug.Log($"Vortex position updated to {worldPosition}");
+            }
+            else
+            {
+                Debug.LogError("vortex GameObject is null in UpdateVortexPosition. Cannot update position.");
+            }        
         }
 
         private IEnumerator DeactivateVortexAfterDelay(float delay)
         {
             yield return new WaitForSeconds(delay);
+            channelTornadoEffect.SetActive(false);
+            vortexEffect.SetActive(false);
+            smokeRingEffect.SetActive(false);
             vortex.SetActive(false);
         }
     }
